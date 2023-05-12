e667aa0b0ef5d1c16a91ff254c3fe26ef13a0ef0
Richhiey <richhiey.thomas@gmail.com>
2023-05-12 17:39:31 +0530

    fixes to tpu estimator for oaf drums

--- magenta/models/onsets_frames_transcription/estimator_spec_util.py
+++ magenta/models/onsets_frames_transcription/estimator_spec_util.py
@@ -226,10 +226,8 @@ def get_estimator_spec(hparams, mode, features, labels, frame_logits,
             staircase=True),
         clip_gradients=hparams.clip_norm,
         summaries=[],
-        optimizer=
-        lambda lr: tf.tpu.CrossShardOptimizer(tf.train.AdamOptimizer(lr)))
-
-    return tf.tpu.estimator.TPUEstimatorSpec(
+        optimizer=lambda lr: tf.train.AdamOptimizer(lr))
+    return tf.estimator.tpu.TPUEstimatorSpec(
         mode=mode, loss=loss, train_op=train_op)
   elif mode == tf_estimator.ModeKeys.EVAL:
     metric_ops = {k: tf.metrics.mean(v) for k, v in metrics_values.items()}
